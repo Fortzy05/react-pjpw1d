8be78e1d5723fc10f8f2be1e75cf9c325ef94c82
Fortzy05 <39365492+Fortzy05@users.noreply.github.com>
2022-09-28 17:43:52 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import faker from "faker";
 import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="ui container comment">
+      <div className="comment">
+        <div>
+        <a href="/" className="avatar">
+          <img src="" alt="avatar"/>
+        </a>
+        </div>
+        <div className="content">
+          <a href='/' className="author">Sam</a>
+          <div className="metadata">
+          <span className="date">Today at 6:00pm</span>
+          </div>
+          <div className="text">This is a nice post</div>
+        </div>
+      </div>
+      
     </div>
   );
 }
