c5f089ed93098d3f71025ed09521d2fe0418a31d
Fortzy05 <39365492+Fortzy05@users.noreply.github.com>
2022-09-28 19:48:03 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import CommentDetails from "./CommentDetails";
-import "./style.css";
+import React from 'react';
+import CommentDetails from './CommentDetails';
+import './style.css';
 
 export default function App() {
   return (
     <div className="ui container">
-     <CommentDetails/>
-     <CommentDetails/>
-     <CommentDetails/>
+      <CommentDetails author="Sam" timeAgo="Today at 6:00pm"  post="Nice blog post"/>
+      <CommentDetails author="Nosa" timeAgo="Today at 7:00am" post="Glad to be a part of this"/>
+      <CommentDetails author="Cynthia" timeAgo="Yesterday at 5:00pm" post="Today is a great day"/>
     </div>
   );
 }
--- src/CommentDetails.js
+++ src/CommentDetails.js
@@ -1,27 +1,26 @@
-import React from "react";
+import React from 'react';
 
-
-const CommentDetails = () =>{
-  return( 
+const CommentDetails = (props) => {
+  return (
     <div className="ui comments">
-  <div className="comment">
-  <div>
-  <a href="/" className="avatar">
-    <img src="" alt="avatar"/>
-  </a>
-  </div>
-  <div className="content">
-    <a href='/' className="author">Sam</a>
-    <div className="metadata">
-    <span className="date">Today at 6:00pm</span>
+      <div className="comment">
+        <div>
+          <a href="/" className="avatar">
+            <img src="" alt="avatar" />
+          </a>
+        </div>
+        <div className="content">
+          <a href="/" className="author">
+            {props.author}
+          </a>
+          <div className="metadata">
+            <span className="date">{props.timeAgo}</span>
+          </div>
+          <div className="text">{props.post}</div>
+        </div>
+      </div>
     </div>
-    <div className="text">This is a nice post</div>
-  </div>
-</div>
-</div>
-  )
-}
-
-
+  );
+};
 
 export default CommentDetails;
