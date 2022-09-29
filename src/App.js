f0878c055b9f4e73c04bf4e835240179075895ea
Fortzy05 <39365492+Fortzy05@users.noreply.github.com>
2022-09-29 13:31:34 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import CommentDetails from './CommentDetails';
 import './style.css';
-import ApprovalCard from "./ApprovalCard";
+import ApprovalCard from './ApprovalCard';
 
 export default function App() {
   return (
     <div className="ui container">
-       
-       <CommentDetails
-        author="Sam"
-        timeAgo="Today at 6:00pm"
-        post="Nice blog post"
-      />
-       
-     
+      <ApprovalCard >
+        <CommentDetails
+          author="Sam"
+          timeAgo="Today at 6:00pm"
+          post="Nice blog post"
+        />
+      </ApprovalCard>
+
       <CommentDetails
         author="Nosa"
         timeAgo="Today at 7:00am"
--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 
-
- const ApprovalCard = () =>{
-  return(
+const ApprovalCard = () => {
+  return (
     <div className="ui cards">
       <div className="card">
-      <div class="content">
-    <div class="extra content">
-      <div class="ui two buttons">
-        <div class="ui basic green button">Approve</div>
-        <div class="ui basic red button">Decline</div>
+        <div class="content">Are you sure?</div>
+        <div class="extra content">
+          <div class="ui two buttons">
+            <div class="ui two button">
+              <div className="ui basic green button">Approve</div>
+              <div className="ui basic red button">Reject</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-      </div>
-    </div>
-  )
-}
+  );
+};
 
 export default ApprovalCard;
