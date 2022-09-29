8fbd16d6a2f346bdde5cf2e97ccb58887986bdd5
Fortzy05 <39365492+Fortzy05@users.noreply.github.com>
2022-09-29 13:52:03 +0100

    1 file modified

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -5,7 +5,7 @@ const ApprovalCard = (props) => {
     <div className="ui card">
       <div class="content">{props.children}</div>
       <div class="extra content">
-        <div class="ui two button">
+        <div className="ui two buttons">
           <div className="ui basic green button">Approve</div>
           <div className="ui basic red button">Reject</div>
         </div>
